6b7ac8c836e1ccc97b99544cf45e93862c5d48bb
Andrii Gnatiuk <andrii.gnatiuk@qvantel.com>
2017-12-16 20:08:07 +0200

    hls: extrcting from zip, inserting using Futures

--- src/main/scala/org/gnat/hls/EntryPoint.scala
+++ src/main/scala/org/gnat/hls/EntryPoint.scala
@@ -4,13 +4,14 @@ import java.io.File
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
+import scala.util.Success
 import scalaz.concurrent.Task
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.streaming._
 import io.iteratee.scalaz.task._
 import org.gnat.hls.models.{LocationRepository, UserRepository, VisitRepository}
 import org.gnat.hls.models._
-import org.gnat.hls.utils.TimeUtils._
+import org.gnat.hls.utils.Utils._
 import slick.jdbc.PostgresProfile.api._
 import slick.jdbc.meta.MTable
 
@@ -24,6 +25,9 @@ object EntryPoint extends LazyLogging {
   val tables = Map("users" -> usersRepository.userTableQuery,
                    "locations" -> locationsRepository.locationTableQuery,
                    "visits" -> visitsRepository.visitTableQuery)
+
+  val workingPath =
+    "/Users/dragsa/Documents/HLC/hlcupdocs-master/data/FULL/data_gnat/"
 
   def initTables(): Unit = {
     tables.keys.foreach(tableCreator)
@@ -42,71 +46,107 @@ object EntryPoint extends LazyLogging {
     )
   }
 
-  val arrayProcessor: (Array[Byte] => Array[Byte]) = a => {
+  val objectArrayExtractor: (Array[Byte] => Array[Byte]) = a => {
     if (a.contains(91.toByte)) a.dropWhile(b => !b.equals(91.toByte))
     else if (a.contains(93.toByte)) a.take(a.length - 1)
     else a
   }
 
+  // TODO parametrization is tricky here due to to the way how cicre consumes implicits
   def userDecoder(path: String) =
     readBytes(new File(path))
-      .map(arrayProcessor)
+      .map(objectArrayExtractor)
       .through(byteParser)
       .through(decoder[Task, User])
-      .map(a => {
-        println(a)
-        Await.result(usersRepository.createOne(a), Duration.Inf)
-      })
       .into(takeWhileI(_ => true))
+      .map(a =>
+        usersRepository
+          .createMany(a.toList)
+          .andThen({
+            case Success(_) => logger.info("finished processing file " + path)
+          }))
       .unsafePerformSync
 
   def locationDecoder(path: String) =
     readBytes(new File(path))
-      .map(arrayProcessor)
+      .map(objectArrayExtractor)
       .through(byteParser)
       .through(decoder[Task, Location])
-      .map(a => println(a))
       .into(takeWhileI(_ => true))
+      .map(a =>
+        locationsRepository
+          .createMany(a.toList)
+          .andThen({
+            case Success(_) => logger.info("finished processing file " + path)
+          }))
       .unsafePerformSync
 
-  def visitDecoder(path: String) =
+  def visitDecoder[A](path: String) =
     readBytes(new File(path))
-      .map(arrayProcessor)
+      .map(objectArrayExtractor)
       .through(byteParser)
       .through(decoder[Task, Visit])
-      .map(a => println(a))
       .into(takeWhileI(_ => true))
+      .map(a =>
+        visitsRepository
+          .createMany(a.toList)
+          .andThen({
+            case Success(_) => logger.info("finished processing file " + path)
+          }))
       .unsafePerformSync
 
-  lazy val allFiles =
-    listFiles(new File("/temp/data/"))
-//    listFiles(
-//      new File("/Users/dragsa/Documents/HLC/hlcupdocs-master/data/FULL/data/"))
+  val allFiles = {
+    zippedFolderExtractor(workingPath)
+    listFiles(new File(workingPath))
+    //    listFiles(new File("/temp/data/"))
+  }
 
-  lazy val insertUsers = allFiles
-    .filter(f => f.getName.startsWith("users"))
-    .map(ff => {
-      println(ff)
-      userDecoder(ff.getAbsolutePath)
-    })
-    .into(takeWhileI(_ => true))
-    .unsafePerformSync
+  def insertUsers =
+    Future {
+      allFiles
+        .filter(f =>
+          f.getName.startsWith("users") && f.getName.endsWith(".json"))
+        .map(ff => {
+          logger.info("processing input file " + ff)
+          userDecoder(ff.getAbsolutePath)
+        })
+        .into(takeWhileI(_ => true))
+        .unsafePerformSync
+    }
 
-  lazy val insertLocations = allFiles
-    .filter(f => f.getName.startsWith("locations"))
-    .map(f => locationDecoder(f.getAbsolutePath))
-    .into(takeWhileI(_ => true))
-    .unsafePerformSync
+  def insertLocations =
+    Future {
+      allFiles
+        .filter(f =>
+          f.getName.startsWith("locations") && f.getName.endsWith(".json"))
+        .map(ff => {
+          logger.info("processing input file " + ff)
+          locationDecoder(ff.getAbsolutePath)
+        })
+        .into(takeWhileI(_ => true))
+        .unsafePerformSync
+    }
 
-  lazy val insertVisits = allFiles
-    .filter(f => f.getName.startsWith("visits"))
-    .map(f => visitDecoder(f.getAbsolutePath))
-    .into(takeWhileI(_ => true))
-    .unsafePerformSync
+  def insertVisits =
+    Future {
+      allFiles
+        .filter(f =>
+          f.getName.startsWith("visits") && f.getName.endsWith(".json"))
+        .map(ff => {
+          logger.info("processing input file " + ff)
+          visitDecoder(ff.getAbsolutePath)
+        })
+        .into(takeWhileI(_ => true))
+        .unsafePerformSync
+    }
 
   def main(args: Array[String]): Unit = {
     logger.info("started")
     initTables()
 //    timeCounter(insertUsers, "insert all users")
+    insertUsers
+    insertLocations
+    insertVisits
+    Thread.sleep(6000000)
   }
 }
---
+++ src/main/scala/org/gnat/hls/utils/Utils.scala
@@ -0,0 +1,25 @@
+package org.gnat.hls.utils
+
+import java.io.{FileInputStream, FileOutputStream}
+import java.util.zip.ZipInputStream
+import com.typesafe.scalalogging.LazyLogging
+
+object Utils extends LazyLogging {
+  def timeCounter[R](block: => R, operation: String): R = {
+    val t0 = System.nanoTime()
+    val result = block
+    val t1 = System.nanoTime()
+    logger.info(String.format("Elapsed time for %s: " + (t1 - t0) + "ns", operation))
+    result
+  }
+
+  def zippedFolderExtractor(path: String) = {
+    val zis = new ZipInputStream(new FileInputStream(path + "data.zip"))
+    Stream.continually(zis.getNextEntry).takeWhile(_ != null).foreach { file =>
+      logger.info("extracting " + file)
+      val fout = new FileOutputStream(path + "/" + file.getName)
+      val buffer = new Array[Byte](1024)
+      Stream.continually(zis.read(buffer)).takeWhile(_ != -1).foreach(fout.write(buffer, 0, _))
+    }
+  }
+}
